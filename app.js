7e34808e3071f812431aba22d01886a5a34ed7a0
MakeHoney <makehoney3@gmail.com>
2018-04-23 14:20:33 +0900

    node를 이용하는 것으로 변경

---
+++ app.js
@@ -0,0 +1,20 @@
+var coap        = require('coap')
+  , server      = coap.createServer({ type: 'udp6' })
+
+server.on('request', function(req, res) {
+  res.end('Hello ' + req.url.split('/')[1] + '\n')
+})
+
+// the default CoAP port is 5683
+server.listen(function() {
+  var req = coap.request('coap://[::1]/sample')
+
+  req.on('response', function(res) {
+    res.pipe(process.stdout)
+    res.on('end', function() {
+      process.exit(0)
+    })
+  })
+
+  req.end()
+})
