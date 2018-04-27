e8308d9a5c6d14a454f18eafe717701955a883e0
makehoney <buffalo1332@gmail.com>
2018-04-28 03:14:03 +0900

    cred_push() 함수 추가

--- openwsn-fw-REL-1.8.0/openapps/cred/cred.c
+++ openwsn-fw-REL-1.8.0/openapps/cred/cred.c
@@ -28,7 +28,7 @@ void cred_init() {
 	cred_vars.desc.componentID	= COMPONENT_CRED;
 	cred_vars.desc.callbackRx	= &cred_receive;
 	cred_vars.desc.callbackSendDone = &cred_sendDone;
-	
+
 	cred_vars.timerId = opentimers_start(
 		3000,
 		TIMER_PERIODIC,
@@ -42,6 +42,76 @@ void cred_init() {
 
 void cred_sendDone(OpenQueueEntry_t* msg, owerror_t error) {
 	openqueue_freePacketBuffer(msg);
+}
+
+void cred_push() {
+	OpenQueueEntry_t* pkt;
+	owerror_t outcome;
+	uint8_t numOptions;
+	
+	// don't run if not synch
+	if (ieee154e_isSynch() == FALSE) return;
+
+	// don't run on dagroot
+	if (idmanager_getIsDAGroot()) {
+		opentimers_stop(cred_vars.timerId);
+		return;
+	}
+
+	//=== request a packetBuffer
+
+	// create a CoAP RD packet
+	pkt = openqueue_getFreePacketBuffer(COMPONENT_CRED);
+
+	if (pkt==NULL) {
+		openserial_printError(
+			COMPONENT_CRED,
+			ERR_NO_FREE_PACKET_BUFFER,
+			(errorparameter_t)0,
+			(errorparameter_t)0
+		);
+		openqueue_freePacketBuffer(pkt);
+		return;
+	}
+
+	// take ownership over that packet
+	pkt->creator = COMPONENT_CRED;
+	pkt->owner = COMPONENT_CRED;
+
+	// CoAP payload
+	numOptions = 0;
+	packetfunctions_reserveHeaderSize(pkt,1);
+	pkt->payload[0] = leds_error_isOn();
+
+	// payload marker
+	packetfunctions_reserveHeaderSize(pkt,1);
+	pkt->payload[0] = COAP_PAYLOAD_MARKER;
+
+	// URI-path
+	packetfunctions_reserveHeaderSize(pkt,sizeof(cred_path0)-1);
+	memcpy(pkt->payload,cred_path0,sizeof(cred_path0)-1);
+	packetfunctions_reserveHeaderSize(pkt,1);
+	pkt->payload[0] = (COAP_OPTION_NUM_URIPATH) << 4 | 3;
+	numOptions++;
+
+	//=== send CoAP message
+	// metadata
+	pkt->l4_destination_port = 7654;
+	pkt->l3_destinationAdd.type = ADDR_128B;
+	memcpy(&pkt->l3_destinationAdd.addr_128b[0],&ipAddr_Server,16);
+
+	// send
+	outcome = opencoap_send(
+		pkt,
+		COAP_TYPE_NON,
+		COAP_CODE_REQ_POST,
+		numOptions,
+		&cred_vars.desc );
+
+	if (outcome==E_FAIL) {
+	openqueue_freePacketBuffer(pkt);
+	}
+	return;
 }
 
 owerror_t cred_receive(OpenQueueEntry_t* msg,
