535b0c2e3ee392c4e2164774d43e33810e690a8b
MakeHoney <makehoney3@gmail.com>
2018-04-29 06:04:52 +0900

    udp로 변경

--- socket_test/client.c
+++ socket_test/client.c
@@ -1,68 +1,115 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
-int main(int argc, char **argv)
+#define  BUFF_SIZE   1024
+
+int   main( int argc, char **argv)
 {
-    int send_len;
+   int   sock;
+   int   server_addr_size;
 
-    int client_len;
-    int client_sockfd;
+   struct sockaddr_in   server_addr;
 
-    char buf_in[255];
-    char buf_get[255];
-
-    struct sockaddr_in clientaddr;
-
-    if (argc != 2)
-    {
-        perror("argument error : ");
-        exit(0);
-    }
+   char   buff_rcv[BUFF_SIZE+5];
 
 
-    client_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    clientaddr.sin_family = AF_INET;
-    clientaddr.sin_addr.s_addr = inet_addr("13.209.8.64");
-    // clientaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    clientaddr.sin_port = htons(atoi(argv[1]));
+   sock  = socket( PF_INET, SOCK_DGRAM, 0);
+   
+   if( -1 == sock)
+   {
+      printf( "socket 생성 실패n");
+      exit( 1);
+   }
 
-    client_len = sizeof(clientaddr);
+   memset( &server_addr, 0, sizeof( server_addr));
+   server_addr.sin_family     = AF_INET;
+   server_addr.sin_port       = htons(7777);
+   server_addr.sin_addr.s_addr= inet_addr( "127.0.0.1");
 
-    if (connect(client_sockfd, (struct sockaddr *)&clientaddr, client_len) < 0)
-    {
-        perror("Connect error: ");
-        exit(0);
-    }
-    while(1)
-    {
-        printf("입력 : ");
-        fgets(buf_in, 255, stdin);
+   sendto( sock, argv[1], strlen( argv[1])+1, 0,    // +1: NULL까지 포함해서 전송
+            ( struct sockaddr*)&server_addr, sizeof( server_addr));          
 
-        buf_in[strlen(buf_in) - 1] = '\0';
-        /* for C server */
-        // write(client_sockfd, buf_in, 255);
-
-        /* for node server */
-        send_len = strlen(buf_in);
-        write(client_sockfd, buf_in, send_len);
-
-        if (strncmp(buf_in, "quit", 4) == 0)
-        {
-            close(client_sockfd);
-            exit(0);
-        }
-
-        read(client_sockfd, buf_get, 255);
-        printf("%s\n", buf_get);
-    }
-
-    close(client_sockfd);
-    exit(0);
+   server_addr_size  = sizeof( server_addr);
+   recvfrom( sock, buff_rcv, BUFF_SIZE, 0 , 
+            ( struct sockaddr*)&server_addr, &server_addr_size);
+   printf( "receive: %sn", buff_rcv);
+   close( sock);
+   
+   return 0;
 }
+
+
+
+// #include <sys/types.h>
+// #include <sys/stat.h>
+// #include <sys/socket.h>
+// #include <unistd.h>
+// #include <netinet/in.h>
+// #include <arpa/inet.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+// int main(int argc, char **argv)
+// {
+//     int send_len;
+
+//     int client_len;
+//     int client_sockfd;
+
+//     char buf_in[255];
+//     char buf_get[255];
+
+//     struct sockaddr_in clientaddr;
+
+//     if (argc != 2)
+//     {
+//         perror("argument error : ");
+//         exit(0);
+//     }
+
+
+//     client_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+//     clientaddr.sin_family = AF_INET;
+//     // clientaddr.sin_addr.s_addr = inet_addr("13.209.8.64");
+//     clientaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+//     clientaddr.sin_port = htons(atoi(argv[1]));
+
+//     client_len = sizeof(clientaddr);
+
+//     if (connect(client_sockfd, (struct sockaddr *)&clientaddr, client_len) < 0)
+//     {
+//         perror("Connect error: ");
+//         exit(0);
+//     }
+//     while(1)
+//     {
+//         printf("입력 : ");
+//         fgets(buf_in, 255, stdin);
+
+//         buf_in[strlen(buf_in) - 1] = '\0';
+//         /* for C server */
+//         // write(client_sockfd, buf_in, 255);
+
+//         /* for node server */
+//         send_len = strlen(buf_in);
+//         write(client_sockfd, buf_in, send_len);
+
+//         if (strncmp(buf_in, "quit", 4) == 0)
+//         {
+//             close(client_sockfd);
+//             exit(0);
+//         }
+
+//         read(client_sockfd, buf_get, 255);
+//         printf("%s\n", buf_get);
+//     }
+
+//     close(client_sockfd);
+//     exit(0);
+// }
--- socket_test/server.c
+++ socket_test/server.c
@@ -11,59 +11,38 @@
 #define PORT	7777
 
 int main() {
-	int server_sockfd, client_sockfd;
-	int client_len;
-	int pid;
+	int s;
 
 	struct sockaddr_in6 clientaddr, serveraddr;
+	int client_len = sizeof(clientaddr);
 
 	char buf[255];
 
 	// 소켓 생성
-	if((server_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	if((s = socket(AF_INET6, SOCK_DGRAM, 0)) < 0) {
 		perror("socket error : ");
 		exit(0);
 	}
 
 	memset(&serveraddr, 0x00, sizeof(serveraddr));
-	serveraddr.sin6_family = AF_INET;
+	serveraddr.sin6_family = AF_INET6;
 	serveraddr.sin6_addr = in6addr_any;
 	serveraddr.sin6_port = htons(PORT);
 
 	// 바인딩 (소켓에 이름 연결)
-	if(bind(server_sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) == -1) {
+	if(bind(s, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) == -1) {
 		perror("bind error : "); 
 		exit(0);
 	}
 
-	if(listen(server_sockfd, 5) == -1) {
-		perror("listen error : ");
-		exit(0);
-	}
-
+	/* 출력문 넣어서 이 위치에서 하는 오퍼레이션 확인 */
 	while(1) {
-		client_sockfd = accept(server_sockfd, (struct sockaddr*)&clientaddr, &client_len);
+		memset(buf, '\0', 255);
 
-		if(client_sockfd == -1) {
-			perror("Accept error : ");
-			exit(0);
-		}
-		printf("a client connected\n");
-
-		/* 출력문 넣어서 이 위치에서 하는 오퍼레이션 확인 */
-		while(1) {
-			/* rewind -> 파일 읽기/쓰기 포인터 위치를 처음으로 초기화 */
-			memset(buf, '\0', 255);
-
-			/* 클라이언트가 입력한 내용을 버퍼에 담는다 read는 fd에서 읽어들인 바이트수를 반환 */
-			if(read(client_sockfd, buf, 255) <= 0) {
-				close(client_sockfd);
-				break;
-			}
-			write(client_sockfd, buf, 255);
-			printf("Data: %s\n", buf);
-		}
-		printf("a client disconnected\n");
+		/* 클라이언트가 입력한 내용을 버퍼에 담는다 read는 fd에서 읽어들인 바이트수를 반환 */
+		recvfrom(s, buf, 255, 0, (struct sockaddr*)&clientaddr, &client_len);
+		printf("Data: %s\n", buf);
 	}
-	close(client_sockfd);
+	printf("a client disconnected\n");
+	close(s);
 }
