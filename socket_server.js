49bd4fca4068a09c0440b2584cd8f6dad69b2add
EC2 Default User <ec2-user@ip-172-31-19-163.ap-northeast-2.compute.internal>
2018-04-28 07:14:30 +0000

    node socket server 구현

---
+++ socket_server.js
@@ -0,0 +1,24 @@
+const port = 7777;
+
+var net = require('net');
+var server = net.createServer(function(socket) {
+	console.log('client connected');
+
+	socket.on('close', function() {
+		console.log('client disconnected');
+	});
+
+	socket.on('data', function(data) {
+		data = data.toString();
+		console.log('message from client : ' + data);
+		socket.write('echo from server : ' + data + '\n\0');
+		/* 서버단 local ip */
+		console.log('local = %s:%s', socket.localAddress, socket.localPort);
+		/* 클라이언트 단 공유기(router) ip */
+		console.log('remote = %s:%s', socket.remoteAddress, socket.remotePort);
+	});
+});
+
+server.listen(port, function() {
+	console.log('server listening...');
+});
