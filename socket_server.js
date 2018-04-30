050cc7f9468f5f868c734b7ed9939646b496ebdb
MakeHoney <makehoney3@gmail.com>
2018-04-30 15:37:48 +0900

    node local server 구축

--- socket_server.js
+++ socket_server.js
@@ -1,24 +1,45 @@
-const port = 7777;
+const PORT = 7777;
 
-var net = require('net');
-var server = net.createServer(function(socket) {
-	console.log('client connected');
+var dgram = require('dgram');
 
-	socket.on('close', function() {
-		console.log('client disconnected');
-	});
+var server = dgram.createSocket("udp4");
 
-	socket.on('data', function(data) {
-		data = data.toString();
-		console.log('message from client : ' + data);
-		socket.write('echo from server : ' + data + '\n\0');
-		/* 서버단 local ip */
-		console.log('local = %s:%s', socket.localAddress, socket.localPort);
-		/* 클라이언트 단 공유기(router) ip */
-		console.log('remote = %s:%s', socket.remoteAddress, socket.remotePort);
-	});
+server.on("message", function(msg, rinfo) {
+	console.log("server got : " + msg + "from " +
+		rinfo.address + " : " + rinfo.port);
 });
 
-server.listen(port, '::', function() {
-    console.log('server listening...');
+server.on("listening", function() {
+	var address = server.address();
+	console.log("server listening " + address.address + 
+		" : " + address.port);
 });
+
+server.bind(PORT);
+
+
+
+// const port = 7777;
+
+// var net = require('net');
+// var server = net.createServer(function(socket) {
+// 	console.log('client connected');
+
+// 	socket.on('close', function() {
+// 		console.log('client disconnected');
+// 	});
+
+// 	socket.on('data', function(data) {
+// 		data = data.toString();
+// 		console.log('message from client : ' + data);
+// 		socket.write('echo from server : ' + data + '\n\0');
+// 		/* 서버단 local ip */
+// 		console.log('local = %s:%s', socket.localAddress, socket.localPort);
+// 		/* 클라이언트 단 공유기(router) ip */
+// 		console.log('remote = %s:%s', socket.remoteAddress, socket.remotePort);
+// 	});
+// });
+
+// server.listen(port, '::', function() {
+//     console.log('server listening...');
+// });
