c73b282230fc6f7187ca75ebd8535ff778a1ea0f
MakeHoney <makehoney3@gmail.com>
2018-05-02 01:44:43 +0900

    main server file write 추가

--- main_server.js
+++ main_server.js
@@ -1,12 +1,17 @@
 const PORT = 7777;
 
 var dgram = require('dgram');
+var fs = require('fs');
 
 var server = dgram.createSocket("udp6");
 
 server.on("message", function(msg, rinfo) {
 	console.log("server got : " + msg + " from " +
 		rinfo.address + ":" + rinfo.port);
+
+	fs.appendFile('log.txt', msg + "\n", function(err) {
+		if(err) throw err;
+	});
 });
 
 server.on("listening", function() {
