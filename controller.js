d59cd48217f29399734fe315591ac14420b46c6e
EC2 Default User <ec2-user@ip-172-31-19-163.ap-northeast-2.compute.internal>
2018-05-02 02:28:06 +0000

    불필요한 파일 삭제 및 카카오 readme 수정

--- controller.js
+++ controller.js
@@ -1,6 +1,4 @@
 const PORT = 7777;
-const COAPPORT = 8888;
-const CRAWLERPORT = 9999;
 
 var dgram	= require('dgram'),
 	packet	= require('coap-packet'),
@@ -15,16 +13,9 @@ controller.on("message", function(msg, rinfo) {
 	msg = parse(msg).payload.toString();
 	console.log("controller got : " + msg + " from " + rinfo.address + ":" + rinfo.port);
 
-	if(rinfo.port == CRAWLERPORT) {
-		controller.send(msg, 0, msg.length, COAPPORT, "localhost", function(err, bytes) {
-			console.log("controller sent a crawled message(" + msg + ") to node_coap server")
-		});
-
-	} else {
-		controller.send(msg, 0, msg.length, PORT, "::ffff:13.209.8.64", function(err, bytes) {
-			console.log("controller sent a message(" + msg + ") to main server");
-		});
-	}
+	controller.send(msg, 0, msg.length, PORT, "::ffff:13.209.8.64", function(err, bytes) {
+		console.log("controller sent a message(" + msg + ") to main server");
+	});
 });
 
 controller.on("listening", function() {
