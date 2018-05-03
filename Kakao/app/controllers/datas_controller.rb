4a7fa9f57bf0620997f097e93d9c2511a20e2d5e
makehoney <buffalo1332@gmail.com>
2018-05-03 15:07:21 +0900

    카카오수정

--- Kakao/app/controllers/datas_controller.rb
+++ Kakao/app/controllers/datas_controller.rb
@@ -2,34 +2,110 @@ class DatasController < ApplicationController
 	def keyboard
 		@msg = {
 			type: "buttons",
-			buttons: ["요청 로그 확인"]
+			buttons: ["요청 로그 확인", "병헌", "정연", "태성"]
 		}
 		render json: @msg, status: :ok
 	end
 
 	def chat
-		if File.exist?('/home/ec2-user/iot_coap/log.txt')
-			fd = File.new('/home/ec2-user/iot_coap/log.txt', 'r');
-			fs = fd.stat.size
-			content = fd.sysread(fs)
-		else
-			content = '작성된 로그가 없습니다.'
-		end
+		@res = params[:content]
 
-		content.chomp!
+		if @res.eql?("요청 로그 확인") 
+			if File.exist?('/home/ec2-user/iot_coap/log.txt')
+				fd = File.new('/home/ec2-user/iot_coap/log.txt', 'r');
+				fs = fd.stat.size
+				content = fd.sysread(fs)
+			else
+				content = '작성된 로그가 없습니다.'
+			end
 
-		# @res = params[:content]
-		# @user_key = params[:user_key]
-		@msg = {
-			message: {
-				text: content
-			},
-			keyboard: {
-				type: "buttons",
-				buttons: ["요청 로그 확인"]
+			content.chomp!
+
+			# @res = params[:content]
+			# @user_key = params[:user_key]
+			@msg = {
+				message: {
+					text: content
+				},
+				keyboard: {
+					type: "buttons",
+					buttons: ["요청 로그 확인", "병헌", "정연", "태성"]
+				}
 			}
-		}
-		render json: @msg, status: :ok
-	end
+			render json: @msg, status: :ok
 
+		elsif @res.eql?("병헌")
+			if File.exist?('/home/ec2-user/iot_coap/byunghun.txt')
+				fd = File.new('/home/ec2-user/iot_coap/byunghun.txt', 'r');
+				fs = fd.stat.size
+				content = fd.sysread(fs)
+			else
+				content = '0'
+			end
+
+			content.chomp!
+
+			# @res = params[:content]
+			# @user_key = params[:user_key]
+			@msg = {
+				message: {
+					text: content
+				},
+				keyboard: {
+					type: "buttons",
+					buttons: ["요청 로그 확인", "병헌", "정연", "태성"]
+				}
+			}
+			render json: @msg, status: :ok
+
+		elsif @res.eql?("정연")
+			if File.exist?('/home/ec2-user/iot_coap/jungyeon.txt')
+				fd = File.new('/home/ec2-user/iot_coap/jungyeon.txt', 'r');
+				fs = fd.stat.size
+				content = fd.sysread(fs)
+			else
+				content = '0'
+			end
+
+			content.chomp!
+
+			# @res = params[:content]
+			# @user_key = params[:user_key]
+			@msg = {
+				message: {
+					text: content
+				},
+				keyboard: {
+					type: "buttons",
+					buttons: ["요청 로그 확인", "병헌", "정연", "태성"]
+				}
+			}
+			render json: @msg, status: :ok
+
+
+		elsif @res.eql?("태성")
+			if File.exist?('/home/ec2-user/iot_coap/taesung.txt')
+				fd = File.new('/home/ec2-user/iot_coap/taesung.txt', 'r');
+				fs = fd.stat.size
+				content = fd.sysread(fs)
+			else
+				content = '0'
+			end
+
+			content.chomp!
+
+			# @res = params[:content]
+			# @user_key = params[:user_key]
+			@msg = {
+				message: {
+					text: content
+				},
+				keyboard: {
+					type: "buttons",
+					buttons: ["요청 로그 확인"]
+				}
+			}
+			render json: @msg, status: :ok
+		end
+	end
 end
--- main_server.js
+++ main_server.js
@@ -2,8 +2,10 @@ const PORT = 7777;
 
 var dgram = require('dgram');
 var fs = require('fs');
-
 var server = dgram.createSocket("udp6");
+var byunghun = 0,
+	jungyeon = 0,
+	taesung = 0;
 
 server.on("message", function(msg, rinfo) {
 	console.log("server got : " + msg + " from " +
@@ -12,6 +14,24 @@ server.on("message", function(msg, rinfo) {
 	fs.appendFile('log.txt', msg + "\n", function(err) {
 		if(err) throw err;
 	});
+
+	if(msg.equals('binary counter')) {
+		byunghun++;
+		fs.writeFile('byunghun.txt', byunghun.toString(), 'utf8', function(err) {
+			if(err) throw err;
+		})
+	} else(msg.equals('1 2 3 blink')) {
+		jungyeon++;
+		fs.writeFile('jungyeon.txt', jungyeon.toString(), 'utf8', function(err) {
+			if(err) throw err;
+		})
+	} else(msg.equals('wave 5 blink')) {
+		taesung++;
+		fs.writeFile('taesung.txt', taesung.toString(), 'utf8', function(err) {
+			if(err) throw err;
+		})
+	}
+
 });
 
 server.on("listening", function() {
